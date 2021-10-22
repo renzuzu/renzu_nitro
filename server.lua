93bb333b8948b9d8314ba41f880c0e74894d8fc2
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-10-23 00:36:30 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -202,7 +202,7 @@ end)
 AddEventHandler('entityCreated', function(entity)
   local entity = entity
   Wait(500)
-  if GetEntityPopulationType(entity) == 7 and DoesEntityExist(entity) then
+  if DoesEntityExist(entity) and GetEntityPopulationType(entity) == 7 then
     Wait(4000)
     local plate = GetVehicleNumberPlateText(entity)
     if nitros[plate] and nitros[plate].nitro then
